eb16f79846ade079aab6e24d6bff1b5f8c579163
Andrew Rotert <Andrew.Rotert@trojans.dsu.edu>
2020-06-12 20:42:36 -0500

    Added more error handling. Added comments.

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-﻿const functions = require('firebase-functions');
+﻿//Node JS Modules 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 const jwt = require('jsonwebtoken');
 const https = require('https');
 const express = require('express');
 const engines = require('consolidate');
 var pako = require('pako');
 
+//Build Express App template webpage
 const app = express();
 app.engine('hbs', engines.handlebars);
 app.set('views', './views')
 app.set('view engine', 'hbs')
 
+//Initialize firebase firestore db
 admin.initializeApp({
-    //credential: admin.credential.applicationDefault()
     credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
-//document: date the data was collected. YYYY - MM - DD
+
+//route to db data
 var collections = db.collection("APIAccessData");
 
+//Returns data for a given date
 function getDataFromDate(date) {
     return collections.doc(date).get()
 }
+
+//Sets data for a given date 
 function setDataFromDate(date, appDownloads) {
     collections.doc(date).set({
         downloads: appDownloads
     });
 }
 
+//Credentials needed to connect to API
 let issuerId = "found-in-appstore-connect";
 let apiKeyId = "found-from-private-key-file";
-//let privateKey = fs.readFileSync(`./Apple_Key/AuthKey_${apiKeyId}.p8`); 
+//Either route will work for private key
+//let privateKey = fs.readFileSync(`./Apple_Key/AuthKey_${apiKeyId}.p8`);
 let privateKey = 'copy and paste p8 key as plain text. Include the begin and end lines, along with \n between each line'
 
+//Expires date, set 20m into future
 let now = Math.round((new Date()).getTime() / 1000) + 1199;
-let payload = {
-    "iss": issuerId,
-    "exp": now,
-    "aud": "appstoreconnect-v1"
-};
-let signOptions = {
-    "algorithm": "ES256",
-    header: {
-        "alg": "ES256",
-        "kid": apiKeyId,
-        "typ": "JWT"
-    }
-};
 
-let now = Math.round((new Date()).getTime() / 1000) + 1199;
+//Setup for token
 let payload = {
     "iss": issuerId,
     "exp": now,
@@ -64,7 +62,10 @@ let signOptions = {
 };
 
 let token = jwt.sign(payload, privateKey, signOptions);
+
+//Options for what data to have returned
 var getOptionsYear = function (year) {
+    //YYYY 
     var options19 = {
         host: "api.appstoreconnect.apple.com",
         //path: '/v1/apps/1515131292'
@@ -77,7 +78,6 @@ var getOptionsYear = function (year) {
     };
     return options19;
 };
-
 var getOptionsMonth = function (year, month) {
     //YYYY-MM 
     var options20 = {
@@ -93,6 +93,7 @@ var getOptionsMonth = function (year, month) {
 };
 var getOptionsDaily = function (year, month, day) {
     var options19 = {
+        //YYYY-MM-DD 
         host: "api.appstoreconnect.apple.com",
         //path: '/v1/apps/1515131292' 
         path: `/v1/salesReports?filter[frequency]=DAILY&filter[reportDate]=${year}-${month}-${day}&filter[reportSubType]=SUMMARY&filter[reportType]=SALES&filter[vendorNumber]=88221566&filter[version]=1_0`,
@@ -105,16 +106,17 @@ var getOptionsDaily = function (year, month, day) {
     return options19;
 };
 
+//Renders webpage 
 app.get('/app', (request, response) => {
 
-    //1: app downloads, 3: redownloads, 8: app updates
     var units = 0;
+    //Tables to format dates properly
     var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     var days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 
     var datetime = new Date();
     datetime.setDate(datetime.getDate() - 1);
-    datetime.setHours(datetime.getHours() - 14);
+    datetime.setHours(datetime.getHours() - 6);
     let year = datetime.getFullYear();
     let month = datetime.getMonth();
     let day = datetime.getDate();
@@ -122,39 +124,50 @@ app.get('/app', (request, response) => {
     let date = `${year} - ${months[month]} - ${days[day - 1]}`;
     console.log(date);
 
+    //Checks database for a given date
     getDataFromDate(date)
         .then(doc => {
 
             if (!doc.exists) {
                 console.log('Date not found in database');
                 //Date not found in database
+                //External request to apple API 
                 var req = https.request(getOptionsDaily(year, months[month], days[day - 1]), (res) => {
                     var pti = [0, 0, 0, 0, 0, 0, 0, 0];
 
                     console.log('Status: ' + res.statusCode + '\n');
-                    console.log('Status: ' + res.statusMessage + '\n');
-                    console.log('Headers: ' + JSON.stringify(res.headers) + '\n');
+
                     res.on('data', (chunk) => {
 
-                        var dataFromFile = pako.inflate(chunk, { to: 'string' })
+                        if (res.statusCode === '200') {
 
-                        dataFromFile = dataFromFile.trim();
-                        var dataFromFileArray = dataFromFile.split('\n');
-                        var i;
-                        for (i = 1; i < dataFromFileArray.length; i++) {
-                            var lineDataFromFileArray = dataFromFileArray[i].split('\t');
-                            try {
-                                pti[parseInt(lineDataFromFileArray[6])] += parseInt(lineDataFromFileArray[7]);
+                            var dataFromFile = pako.inflate(chunk, { to: 'string' })
+                                //1: app downloads, 3: redownloads, 8: app updates 
+                            dataFromFile = dataFromFile.trim();
+                            var dataFromFileArray = dataFromFile.split('\n');
+                            var i;
+                            for (i = 1; i < dataFromFileArray.length; i++) {
+                                var lineDataFromFileArray = dataFromFileArray[i].split('\t');
+                                try {
+                                    pti[parseInt(lineDataFromFileArray[6])] += parseInt(lineDataFromFileArray[7]);
+                                }
+                                catch (err) {
+                                    console.log(`Cannot parse type: ${lineDataFromFileArray[6]} Error: ${err}`);
+                                }
                             }
-                            catch (err) {
-                                console.log(`Cannot parse type: ${lineDataFromFileArray[6]} Error: ${err}`);
-                            }
+                            console.log(`Total Units: ${pti[1]}`);
+                            units += pti[1];
+                            setDataFromDate(date, units);
+
+                            response.render('apps', { _year: year, _month: months[month], _day: days[day - 1], text: units, generatedFrom: "App Store Connect API" });
                         }
-                        console.log(`Total Units: ${pti[1]}`);
-                        units += pti[1];
-                        setDataFromDate(date, units);
-
-                        response.render('apps', { _year: year, _month: months[month], _day: days[day - 1], text: units, generatedFrom: "App Store Connect API" });
+                        else {
+                            response.render('apps', { _year: year, _month: months[month], _day: days[day - 1], text: res.statusMessage, generatedFrom: "Failed to connect to App Store Connect API. Please refresh, or try again later." });
+                            console.log(`Status Message: ${res.statusMessage} \n`);
+                            console.log(`Request Date: Y:${year} M:${months[month]} D:${days[day - 1]} H:${datetime.getHours()}`);
+                            console.log(`Expires: ${now}`);
+                            console.log(`Headers: ${JSON.stringify(res.headers)}\n`);
+                        }
                     });
 
                 });
@@ -176,7 +189,17 @@ app.get('/app', (request, response) => {
 
 
 });
-
+//Google Cloud Functions
 exports.app = functions.https.onRequest(app);
 
 
+/*
+ * Following lines are redacted for privacy and security
+
+let issuerId = "found-in-appstore-connect";
+let apiKeyId = "found-from-private-key-file";
+//Either route will work for private key
+let privateKey = fs.readFileSync(`./Apple_Key/AuthKey_${apiKeyId}.p8`);
+let privateKey = 'copy and paste p8 key as plain text. Include the begin and end lines, along with \n between each line'
+
+*/
